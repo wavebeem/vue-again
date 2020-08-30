f15a700bb0b2c5a1591f89d803e33ef860c78c74
Brian Mock <brian@mockbrian.com>
2020-08-30 14:55:51 -0700

    okie dokie

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,7 +95,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
 
 /**
  * Hello World
@@ -106,7 +106,13 @@ export default defineComponent({
     msg: {
       type: String,
       required: true
-    }
+    },
+    x: Number
+  },
+  setup(/* props */) {
+    onMounted(() => {
+      console.log("Hello, world!");
+    });
   }
 });
 </script>
